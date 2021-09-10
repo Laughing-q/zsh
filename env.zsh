01a21c1474741a3e22574d2916a88eebcf7c99d0
Laughing <61612323+Laughing-q@users.noreply.github.com>
2021-09-10 23:00:46 +0800

    Update env.zsh

--- env.zsh
+++ env.zsh
@@ -2,3 +2,12 @@ export EDITOR=nvim
 
 export PATH="/home/laughing/anaconda3/bin:$PATH"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/laughing/TensorRT-7.1.3.4/lib
+
+source /opt/anaconda/bin/activate root
+eval "$(perl -I $HOME/perl5/lib/perl5 -Mlocal::lib)"
+xmodmap ~/.xmodmap
+
+export GOROOT=/usr/lib/go
+export GOPATH=~/workspace/go
+export GOBIN=~/workspace/go/gobin
+export PATH=$PATH:$GOROOT/bin:$GOBIN
