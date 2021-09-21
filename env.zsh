34f3abe6258bdb9ac3b0a4008ed6f80eb68481e9
Laughing-q <1185102784@qq.com>
2021-09-21 16:24:19 +0800

    update

--- env.zsh
+++ env.zsh
@@ -1,13 +1,29 @@
 export EDITOR=nvim
-
-export PATH="/home/laughing/anaconda3/bin:$PATH"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/laughing/TensorRT-7.1.3.4/lib
+export BROWSER=google-chrome-stable
+# 
+# export PATH="/home/laughing/anaconda3/bin:$PATH"
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/laughing/TensorRT-7.1.3.4/lib
 
 source /opt/anaconda/bin/activate root
 eval "$(perl -I $HOME/perl5/lib/perl5 -Mlocal::lib)"
-xmodmap ~/.xmodmap
+# xmodmap ~/.config/zsh/.xmodmap  # write to ~/.xinitrc
+# 
+# export GOROOT=/usr/lib/go
+# export GOPATH=~/workspace/go
+# export GOBIN=~/workspace/go/gobin
+# export PATH=$PATH:$GOROOT/bin:$GOBIN
+#
+# export GTK_IM_MODULE=ibus
+# export XMODIFIERS=@im=ibus
+# export QT_IM_MODULE=ibus
 
-export GOROOT=/usr/lib/go
-export GOPATH=~/workspace/go
-export GOBIN=~/workspace/go/gobin
-export PATH=$PATH:$GOROOT/bin:$GOBIN
+export PATH=$PATH:/home/laughing/.local/bin
+export TERMINFO=/usr/share/terminfo
+#
+#
+# systemctl start runsunloginclient.service
+
+# proxy
+export ALL_PROXY=socks5://127.0.0.1:7890
+# export http_proxy=http://127.0.0.1:7890
+# export https_proxy=https://127.0.0.1:7890
