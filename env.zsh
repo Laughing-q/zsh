358e521b6f7b3101fc275bb8db16b7b446d83f12
Laughing-q <1185102784@qq.com>
2021-09-22 14:53:02 +0800

    update

--- env.zsh
+++ env.zsh
@@ -1,9 +1,10 @@
 export EDITOR=nvim
+export BROWSER=google-chrome
 
 export PATH="/home/laughing/anaconda3/bin:$PATH"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/laughing/TensorRT-7.1.3.4/lib
 
-source /opt/anaconda/bin/activate root
+source /home/laughing/anaconda3/bin/activate root
 eval "$(perl -I $HOME/perl5/lib/perl5 -Mlocal::lib)"
 xmodmap ~/.xmodmap
 
