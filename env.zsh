ab1ac51fdbed23e911350a86b81c05007094d6dd
Laughing-q <1185102784@qq.com>
2021-09-22 14:54:00 +0800

    update

--- env.zsh
+++ env.zsh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/laughing/TensorRT-7.1.3.4/lib
 
 source /home/laughing/anaconda3/bin/activate root
 eval "$(perl -I $HOME/perl5/lib/perl5 -Mlocal::lib)"
-xmodmap ~/.xmodmap
+xmodmap ~/.config/zsh/.xmodmap
 
 export GOROOT=/usr/lib/go
 export GOPATH=~/workspace/go
