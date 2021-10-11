7411237ac94a199cd4775c4dfae27d197ec60b91
Laughing-q <1185102784@qq.com>
2021-10-11 11:49:32 +0800

    add go path

--- env.zsh
+++ env.zsh
@@ -2,6 +2,7 @@ export EDITOR=nvim
 export BROWSER=google-chrome
 
 export PATH="/home/laughing/anaconda3/bin:$PATH"
+export PATH=$PATH:/usr/local/go/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/laughing/TensorRT-7.1.3.4/lib
 
 source /home/laughing/anaconda3/bin/activate root
