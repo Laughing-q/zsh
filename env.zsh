e30ab330342132a9486d8f303de0feea408609cf
Laughing-q <1185102784@qq.com>
2024-07-08 19:21:15 +0800

    update

--- env.zsh
+++ env.zsh
@@ -44,8 +44,8 @@ export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 
 # proxy
 export ALL_PROXY=socks5://127.0.0.1:7890
-export http_proxy=http://127.0.0.1:7890
-export https_proxy=https://127.0.0.1:7890
+# export http_proxy=http://127.0.0.1:7890
+# export https_proxy=https://127.0.0.1:7890
 export LF_ICONS="di=📁:\
 fi=📃:\
 tw=🤝:\
