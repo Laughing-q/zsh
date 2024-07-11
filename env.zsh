5b2e41931ba0390346b00233a427425c2259e0dc
Laughing-q <1185102784@qq.com>
2024-07-11 13:40:31 +0800

    update proxy

--- env.zsh
+++ env.zsh
@@ -43,7 +43,8 @@ export FZF_DEFAULT_OPTS="--bind=ctrl-k:down,ctrl-i:up"
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 
 # proxy
-export ALL_PROXY=socks5://127.0.0.1:7890
+# export ALL_PROXY=socks5://127.0.0.1:7890
+export ALL_PROXY=http://127.0.0.1:7890
 # export http_proxy=http://127.0.0.1:7890
 # export https_proxy=https://127.0.0.1:7890
 export LF_ICONS="di=📁:\
